fece59bbb62bdf343066a25e1122f2ecaaa1ff20
Sebastian Nowacki <sebastian.nowacki@o2.pl>
2026-04-02 21:41:33 +0200

    added create_user serializer and view

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from users import serializers
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserCreateSerializer
+    permission_classes = [AllowAny]
