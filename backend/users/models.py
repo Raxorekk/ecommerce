36211af43b7f6741a59102974eb05ce5396816cc
Sebastian Nowacki <sebastian.nowacki@o2.pl>
2026-04-02 22:13:48 +0200

    added custom user manager to use email address instead of username and prevent errors while creating super user, added date_of_birth to user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,14 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+        
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(
+            email,
+            password=password,
+            **extra_fields
+        )
+        
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
     email = models.EmailField(unique=True, blank=False, null=False)
+    date_of_birth = models.DateField()
     phone_number = PhoneNumberField(blank=False, null=False)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number', 'date_of_birth']
     username = None
+    
+    objects = CustomUserManager()
