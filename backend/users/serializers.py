46d2bfa2e21796ba3ee1d757ba29a6b0d8f9d25c
Sebastian Nowacki <sebastian.nowacki@o2.pl>
2026-04-02 21:52:59 +0200

    added create_user serializer and view

---
+++ backend/users/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from users.mixins import PasswordValidationMixin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserCreateSerializer(PasswordValidationMixin, serializers.ModelSerializer):
+    verify_password = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "password",
+            "verify_password",
+            "email",
+            "phone_number",
+            "first_name",
+            "last_name",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        validated_data.pop("verify_password")
+
+        user = User.objects.create_user(**validated_data)
+
+        return user
