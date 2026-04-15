20674228e52bcb434922b892b2747755d0c6b424
Sebastian Nowacki <sebastian.nowacki@o2.pl>
2026-04-15 23:34:52 +0200

    added products endpoint

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,7 +1,31 @@
 from rest_framework import serializers
 from . import models
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ["name", "slug"]
+        
+
+class SpecificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Specification
+        fields = ["id", "name", "category", "type"]
+        
+
+class SpecificationValueSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='specification.name')
+    
+    class Meta:
+        model = models.ProductSpecificationValue
+        fields = ["name", "value"]
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    specification_values = SpecificationValueSerializer(many=True)
+    
     class Meta:
         model = models.Product
-        fields = ['id', 'name', 'description', 'price', 'category', 'specifications', 'specification_values']
+        fields = ['id', 'name', 'description', 'price', 'category', 'specification_values', "slug", "reviews", "product_img"]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.pagination import PageNumberPagination
 from . import models
 from . import serializers
 
 # Create your views here.
 
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 30
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
 class ProductViewSet(ModelViewSet):
-    queryset = models.Product.objects.all()    
+    queryset = models.Product.objects.select_related("category").prefetch_related("specification_values__specification").all()    
     serializer_class = serializers.ProductSerializer
+    pagination_class = StandardResultsSetPagination
+    lookup_field = "slug"
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["slug", "name"]
+    ordering_fields = ["name", "price"]
+    
     
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
